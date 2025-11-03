2f5677df09bf7f1f7dbfb508e2686fc48e2d66c0
thanh2122005 <thanh2122005@gmail.com>
2025-11-03 13:50:04 +0700

    Thêm mới, xây dựng khung cho module NguoiDung

---
+++ Controllers/NguoiDungController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BaiMoiiii.BUS;
+using BaiMoiiii.MODEL;
+
+namespace BaiMoiiii.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NguoiDungController : ControllerBase
+    {
+        private readonly NguoiDungBUS _bus;
+
+        public NguoiDungController(NguoiDungBUS bus)
+        {
+            _bus = bus;
+        }
+
+
+    }
+}
---
+++ DAL/NguoiDungDAL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using BaiMoiiii.MODEL;
+
+namespace BaiMoiiii.DAL
+{
+    public class NguoiDungDAL
+    {
+        private readonly string _conn;
+
+        public NguoiDungDAL(IConfiguration config)
+        {
+            _conn = config.GetConnectionString("DefaultConnection");
+        }
+
+        
+    }
+}
